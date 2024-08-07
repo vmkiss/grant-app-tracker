231717e15addccfa69c2a6913684676923904488
vmkiss <veronika.kiss17@gmail.com>
2024-08-07 12:36:22 -0700

    add backend and frontend folders to app folder

---
+++ app/backend/grants-controller.mjs
@@ -0,0 +1,104 @@
+// Controllers for Grants Collection
+
+import 'dotenv/config';
+import express from 'express';
+import * as grants from './grants-model.mjs';
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+
+// Create grant controller
+app.post ('/grants', (req, res) => {
+    grants.createGrant(
+        req.body.foundation,
+        req.body.notes,
+        req.body.date,
+        req.body.ask,
+        req.body.award,
+        req.body.currStatus
+    )
+    .then(grant => {
+        console.log(`${grant.foundation} grant was added to the collection.`)
+        res.status(201).json(grant);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(400).json({ Error: 'Grant could not be added to collection'});
+    });
+})
+
+// Retrieve grant controller
+app.get('/grants', (req, res) => {
+    grants.retrieveGrants()
+        .then(grants => {
+            if (grants !== null) {
+                console.log('All grants successfully retrieved')
+            } else {
+                res.status(404).json({ Error: 'No grants found' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ Error: 'Grants could not be retrieved' });
+        });
+});
+
+// Retrieve grant controller by id
+app.get('/grants/:_id', (req, res) => {
+    grants.retrieveGrantByID(req.params._id)
+        .then(grant => {
+            if (grant != null) {
+                console.log(`${grant.foundation} grant was retrieve by its ID`)
+                res.json(grant);
+            } else {
+                res.status(404).json({ Error: 'No grants found'})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ Error: 'Grant could not be retrieved by its ID' });
+        });
+});
+
+// Update grant controller
+app.put('/grants/:_id', (req, res) => {
+    grants.updateGrant(
+        req.params._id,
+        req.body.foundation,
+        req.body.notes,
+        req.body.date,
+        req.body.ask,
+        req.body.award,
+        req.body.currStatus
+    )
+        .then(grant => {
+            console.log(`${grant.foundation} grant successfully updated`);
+            res.json(grant);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ Error: 'Grant could not be updated' });
+        });
+});
+
+// Delete grant controller
+app.delete('/grants/:_id', (req, res) => {
+    grants.deleteGrantByID(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                console.log(`Based on its ID, ${deletedCount} grant was deleted.`);
+                res.status(200).send({ Success: `${deletedCount} grant was deleted based on its ID` });
+            } else {
+                res.status(404).json({ Error: 'Grant could not be deleted' })
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.send({ Error: 'Grant could not be deleted based on ID' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ app/backend/grants-model.mjs
@@ -0,0 +1,95 @@
+// Models for Grants collection
+
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+// Establish database connection
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+
+// Confirm database connection
+db.once("open", (err) => {
+    if (err) {
+        res.status(500).json({Error: 'Connection to database failed'});   
+    } else {
+        console.log('Success: Successfully connected to database');
+    }
+});
+
+// Define grant collection's schema
+const grantSchema = mongoose.Schema({
+    foundation: { type: String, required: true },
+    notes: { type: String, required: true },
+    date: { type: String, required: true },
+    ask: { type: String, required: true },
+    award: { type: String, required: true },
+    currStatus: { type: String, required: true }
+});
+
+// Define Grants model and compile from schema
+const grants = mongoose.model('Grants', grantSchema);
+
+// Create model
+const createGrant = async (foundation, notes, date, ask, award, currStatus) => {
+    const grant = new grants({
+        foundation: foundation,
+        notes: notes,
+        date: date,
+        ask: ask,
+        award: award,
+        currStatus: currStatus
+    });
+    return grant.save();
+}
+
+// Retrieve models
+
+// Retrieve all grants
+const retrieveGrants = async () => {
+    const query = grants.find();
+    return query.exec();
+}
+
+// Retrieve grant by id
+const retrieveGrantByID = async (_id) => {
+    const query = grants.findById({_id: _id});
+    return query.exec();
+}
+
+// Delete model by id
+const deleteGrantByID = async (_id) => {
+    const result = await grants.deleteOne({_id: _id});
+    return result.deletedCount;
+};
+
+// Edit model
+const updateGrant = async (_id, foundation, notes, date, ask, award, currStatus) => {
+    const result = await grants.replaceOne({_id: _id}, {
+        foundation: foundation,
+        notes: notes,
+        date: date,
+        ask: ask,
+        award: award,
+        currStatus: currStatus
+    });
+    return {
+        _id: _id,
+        foundation: foundation,
+        notes: notes,
+        date: date,
+        ask: ask,
+        award: award,
+        currStatus: currStatus
+    }
+}
+
+export { createGrant, retrieveGrants, retrieveGrantByID, updateGrant, deleteGrantByID }
+
+
+
+
+
+
