0e866fe61bec444cabe2a714cc91a2c041e43b3c
vmkiss <veronika.kiss17@gmail.com>
2024-08-07 13:27:20 -0700

    define component that returns a single grant's info as a table row

---
+++ app/frontend/src/components/Grant.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MdDeleteForever, MdEdit } from 'react-icons/md';
+
+// Return table row displaying information for one grant and edit/delete icons
+function Grant({ grant, onEdit, onDelete }) {
+    return (
+        <tr>
+            <td>{grant.foundation}</td>
+            <td>grant.notes</td>
+            <td>grant.date</td>
+            <td>grant.ask</td>
+            <td>grant.award</td>
+            <td>grant.currStatus</td>
+
+            <td><MdDeleteForever onClick={() => onDelete(grant._id)} /></td>
+            <td><MdEdit onClick={() => onEdit(grant)} /></td>
+        </tr>
+    );
+}
+
+export default Grant;
