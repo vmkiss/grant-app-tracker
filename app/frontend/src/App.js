0dd5b87dd2db07f2c1615e25bb9e4181bef7894b
vmkiss <veronika.kiss17@gmail.com>
2024-08-09 16:39:53 -0700

    add routes for main page, add grant page, and edit grant page to routes

---
+++ app/frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import GrantsPage from './pages/GrantsPage';
+import AddGrantPage from './pages/AddGrantPage';
+import EditGrantPage from './pages/EditGrantPage';
+
+// Define function that renders the content in Routes
+function App() {
+
+    const [grant, setGrantToEdit] = useState([])
+
+    return (
+        <>
+        <BrowserRouter>
+
+            <header>
+                <h1>Grant Application Tracker</h1>
+            </header>
+
+            <main>
+                <section>
+                    <Routes>
+                    <Route path="/" element={<GrantsPage setGrant={setGrantToEdit}/>} />
+                    <Route path="/create" element={<AddGrantPage />} /> 
+                    <Route path="/update" element={<EditMoviePage grantToEdit={grant} />} />
+                        
+                    </Routes>
+                </section>
+            </main>
+        </BrowserRouter>
+>        </>
+    )
+
+}
