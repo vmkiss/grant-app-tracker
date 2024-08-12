a0defa6e9068076416415769d1fb83d23b04d653
vmkiss <veronika.kiss17@gmail.com>
2024-08-11 17:22:29 -0700

    style grants table

--- app/frontend/src/components/GrantList.js
+++ app/frontend/src/components/GrantList.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Grant from './Grant';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // Return table with list of all grants
 function GrantList({ grants, onDelete, onEdit }) {
     return(
-        <table id="grants">
-            <thead>
-                <tr>
+        <table id="grants" class="table table-hover">
+            <thead class="table-group-divider">
+                <tr class= "table-success table-group-divider">
                     <th>Foundation Name</th>
                     <th>Notes</th>
                     <th>Due Date</th>
                     <th>Ask Amount</th>
                     <th>Award Amount</th>
                     <th>Current Status</th>
+                    <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
