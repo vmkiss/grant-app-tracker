a3c8288427ed69a2f12920225c5145e78da36341
vmkiss <veronika.kiss17@gmail.com>
2024-08-16 14:27:49 -0700

    import loginUser and signupUser functions and add them to respective routes

--- app/backend/user-routes.js
+++ app/backend/user-routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
+import { signupUser } from './user-controller.mjs'
+import { loginUser } from './user-controller.mjs'
+
 const router = express.Router()
 
 // Login route
-router.post('/login', () => {})
+router.post('/login', loginUser)
 
 // Sign up route
-router.post('/signup', () => {})
+router.post('/signup', signupUser)
 
 module.exports = router
