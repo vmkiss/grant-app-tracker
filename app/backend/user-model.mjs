0b3eba03dffb04424e38222280c97b61ff265f02
vmkiss <veronika.kiss17@gmail.com>
2024-08-16 16:21:03 -0700

    validate email and password

--- app/backend/user-model.mjs
+++ app/backend/user-model.mjs
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
+import validator from 'validator';
 
 
 // Establish database connection
@@ -34,6 +35,18 @@ const userSchema = mongoose.Schema({
 
 // Create static sign up method
 userSchema.statics.signup = async function(email, password) {
+
+    // Validation
+    if (!email || !password) {
+        throw Error('All fields must be completed')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Invalid email')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password too weak')
+    }
+
     // Check if user's email is unique - raise error if not
     const exists = await this.findOne({ email });
     if (exists) {
