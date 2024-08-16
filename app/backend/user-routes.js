e31896dfcd99f613c327a9a20a91cb7cfb77d4e8
vmkiss <veronika.kiss17@gmail.com>
2024-08-16 14:13:03 -0700

    outline routes for user login and sign up

---
+++ app/backend/user-routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+// Login route
+router.post('/login', () => {})
+
+// Sign up route
+router.post('/signup', () => {})
+
+module.exports = router
