7f95c6d4c9ffbc541357d1dd2a9f76c385718351
vmkiss <veronika.kiss17@gmail.com>
2024-08-16 14:22:45 -0700

    define and export functions to login and sign up user

--- app/backend/user-controller.js
+++ app/backend/user-controller.js
@@ -1,4 +1,11 @@
 // login user
-
+const loginUser = async (req, res) => {
+    res.json({ Message: 'Login user' })
+}
 
 // sign up user
+const signupUser = async (req, res) => {
+    res.json({ Message: 'Sign up user' })
+}
+
+module.exports = { signupUser, loginUser }
