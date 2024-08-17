1a9d31ebe2ee32c162277c024a1c3151034a1edf
vmkiss <veronika.kiss17@gmail.com>
2024-08-16 19:27:44 -0700

    create sign up form

---
+++ app/frontend/src/pages/SignUp.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <form onSubmit={}>
+            <h2>Sign up</h2>
+            <label>Email:</label>
+            <input
+                type="email"
+                onChange={e => setEmail(e.target.value)}
+            />
+            <label>Password:</label>
+            <input
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+            />
+        </form>
+    )
+}
