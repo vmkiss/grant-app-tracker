997efe64025dc64755969e66d57fafae430d15ae
vmkiss <veronika.kiss17@gmail.com>
2024-08-16 19:49:12 -0700

    add Navbar component to App.js

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -7,6 +7,7 @@ import AddGrantPage from './pages/AddGrantPage';
 import EditGrantPage from './pages/EditGrantPage';
 import Login from './pages/LogIn';
 import Signup from './pages/SignUp';
+import Navbar from './components/Navbar';
 
 // Define function that renders the content in Routes
 function App() {
@@ -16,7 +17,7 @@ function App() {
     return (
         <>
         <BrowserRouter>
-
+            <Navbar />
             <header>
                 <h1 class="border border-3 mt-3 py-3 px-5">Grant Application Tracker</h1>
             </header>
