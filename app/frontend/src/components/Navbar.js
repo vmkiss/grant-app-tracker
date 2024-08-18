7a2bdc7c17afa89c3ac8df34ecae38fb0c530e5d
vmkiss <veronika.kiss17@gmail.com>
2024-08-17 18:02:04 -0700

    display log in and sign up buttons when user is logged out and log out button when user is logged in

--- app/frontend/src/components/Navbar.js
+++ app/frontend/src/components/Navbar.js
@@ -1,27 +1,37 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogOut'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
     const { logout } = useLogout()
+    const { user } = useAuthContext()
 
     const handleClick = () => {
         logout()
     }
 
     return (
-        <header>
-            <div class="d-flex justify-content-end">
-                <Link to="/login">
-                    <button class="btn btn-success btn-lg mx-2 mt-2">Log in</button>
-                </Link>
-                <nav>
+        <header><div class="d-flex justify-content-end">
+            <nav>
+                {user && (
                     <div>
+                        <span>{user.email}</span>
                         <button class="btn btn-success btn-lg mx-2 mt-2" onClick={handleClick}>Log out</button>
                     </div>
+                )}
+                {!user && (
+                    <div>
+                        <Link to="/login">
+                        <  button class="btn btn-success btn-lg mx-2 mt-2">Log in</button>
+                        </Link>
+                        <Link to="/signup">
+                            <button class="btn btn-success btn-lg mx-2 mt-2">Sign up</button>
+                        </Link>
+                    </div>
+                    )}
                 </nav>
-                <Link to="/signup">
-                    <button class="btn btn-success btn-lg mx-2 mt-2">Sign up</button>
-                </Link>
+                
+        
             </div>
         </header>
     )
