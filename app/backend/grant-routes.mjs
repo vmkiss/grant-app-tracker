dd8c1faff9de47164bf31e6eb3999b5ec6540a4c
vmkiss <veronika.kiss17@gmail.com>
2024-08-18 15:06:35 -0700

    move retrieve grants function to grants-controller and use in grant-routes

--- app/backend/grant-routes.mjs
+++ app/backend/grant-routes.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createGrant } from './grants-controller.mjs';
+import { createGrant, retrieveAll } from './grants-controller.mjs';
 
 const grantRoutes = express.Router()
 
 // Create grant route
 grantRoutes.post('/create', createGrant)
+
+// Retrieve all grants route
+grantRoutes.get('/all', retrieveAll)
 
 export { grantRoutes }
--- app/backend/grants-controller.mjs
+++ app/backend/grants-controller.mjs
@@ -1,5 +1,6 @@
 import * as grants from './grants-model.mjs';
 
+// Create grant controller
 const createGrant = (req, res) => {
     grants.createGrant(
         req.body.foundation,
@@ -19,4 +20,21 @@ const createGrant = (req, res) => {
     });
 }
 
-export { createGrant }
+// Retrieve all grants controller
+const retrieveAll = (req, res) => {
+    grants.retrieveGrants()
+        .then(grants => {
+            if (grants !== null) {
+                console.log('All grants successfully retrieved')
+                res.json(grants);
+            } else {
+                res.status(404).json({ Error: 'No grants found' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ Error: 'Grants could not be retrieved' });
+        });
+}
+
+export { createGrant, retrieveAll }
