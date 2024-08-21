f2b83c29a8be09186b991f20e27e18b125181aee
vmkiss <veronika.kiss17@gmail.com>
2024-08-20 17:16:54 -0700

    style home page logo and log in/sign up buttons

--- app/frontend/src/pages/HomePage.js
+++ app/frontend/src/pages/HomePage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export const HomePage = () => {
     return (
-        <div>Grant Application Tracker Homepage</div>
+        <div id="home-div" className="border border-3">
+            <img src="GrantTracker.png" />
+            <div className="btn-group-vertical" role="group">
+                <Link to="/login">
+                    <button className="btn btn-success btn-lg mx-5 mt-2 mb-5">Log in</button>
+                </Link>
+                <Link to="/signup">
+                    <button className="btn btn-success btn-lg mx-5 mt-2">Sign up</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
