dc02a941347c8198b0476b79c56479533c09c46e
vmkiss <veronika.kiss17@gmail.com>
2024-09-21 18:10:36 -0700

    create container with logo image for login box on homepage

--- app/frontend/src/pages/HomePage.js
+++ app/frontend/src/pages/HomePage.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom'
 
 export const HomePage = () => {
     return (
-        <div id="home-div" className="border border-3">
-            <img src="GrantTracker.png" alt="logo of hands holding heart with dollar sign"/>
-            <div className="btn-group-vertical" role="group">
-                <Link to="/login">
-                    <button className="btn btn-success btn-lg mx-5 mt-2 mb-5">Log in</button>
-                </Link>
-                <Link to="/signup">
-                    <button className="btn btn-success btn-lg mx-5 mt-2">Sign up</button>
-                </Link>
+        <div id="home-div" className="container d-flex justify-content-center align-items-center min-vh-100">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="featured-img">
+                        <img src="GrantTracker.png" className="img-fluid" alt="logo of hands holding heart with dollar sign"/>
+                    </div>
+                </div>
+                <div className="col-md-6"></div>
             </div>
         </div>
     )
